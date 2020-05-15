c7bb6c2218687a4ca98d4bdb3d029e9423828ea2
MilanP5G <milan_parmar@hotmail.co.uk>
2020-05-15 14:31:25 +0100

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
+
+  def user_attributes=(user_attribute)
+    self.user = User.find_or_create_by(username: user_attribute[:username]) unless user_attribute[:username].blank?
+  end
 
 end
