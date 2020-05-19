0764712255991927d1c0145d9bee9add24fc1a4e
Bouloute <annaick1995@gmail.com>
2020-05-19 17:14:27 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "Bob", email: "Bob@bob.bob")
+User.create(username: "Bouloutifull", email: "annaick1995@free.fr")
+Category.create(name: 'Cool')
