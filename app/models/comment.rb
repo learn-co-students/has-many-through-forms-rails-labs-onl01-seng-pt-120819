7411496a657342fdb761c9fc71aabf9f655f403c
github username <github email address>
2020-05-20 17:45:47 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,16 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
 
+
+  # def empty
+
+  # end
 end
+
+
+# <%= form_for @post do |f| %>
+#   <%= f.collection_select :person_id, Author.all, :id, :name_with_initial, prompt: true %>
+#   <%= f.submit %>
+# <% end %>
